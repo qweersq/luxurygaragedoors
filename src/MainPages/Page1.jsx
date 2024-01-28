5bd9fbb2cb273cc4547ce4a807270920451b4e63
Hadad Al Akbar <75150256+qweersq@users.noreply.github.com>
2024-01-28 16:48:43 +0700

    add page 3

--- src/MainPages/Page1.jsx
+++ src/MainPages/Page1.jsx
@@ -9,7 +9,7 @@ function Page1(props) {
 
                 {/* Text */}
                 <div className='absolute inset-0 flex flex-col justify-center items-center px-10 text-center text-white'>
-                    <h1 className='font-poppins text-5xl font-medium uppercase leading-tight'>
+                    <h1 className='text-5xl font-medium uppercase leading-tight'>
                         Elevate Your Space with
                         <span className='text-[#BD261F] ml-3'>Luxury Garage Doors</span>
                     </h1>
@@ -19,7 +19,7 @@ function Page1(props) {
 
                     <div className='flex gap-3 items-center justify-center mt-6'>
                         <button className='w-190 h-61 rounded-full bg-white bg-opacity-90 px-4 py-2 hover:bg-opacity-50 transition duration-300'>
-                            <p className='text-black font-poppins font-medium'>About Us</p>
+                            <p className='text-black font-medium'>About Us</p>
                         </button>
                         <button className='w-190 h-61 rounded-full border border-white bg-opacity-90 px-4 py-2 hover:bg-gray-800 hover:border-gray-800 transition duration-300'>
                             <p className='text-white font-poppins font-medium hover:opacity-50 transition duration-300'>Request A Quote</p>
--- src/MainPages/Page3.jsx
+++ src/MainPages/Page3.jsx
@@ -1,19 +1,29 @@
 import React from 'react';
+import Garagedoorman from '../assets/images/garagedoorman.png';
+import { FaCircleArrowRight } from "react-icons/fa6";
+
 
 function Page3() {
     return (
         <div>
-            <div className='bg-[#2A2A2A] flex gap-10'>
-                <div className='flex flex-col'>
-                    <div>
-                        <span className="w-83 h-3 bg-gray-300"></span>
-                        <h3>About Us</h3>
+            <div className='bg-[#2A2A2A] flex gap-10 text-white p-40 max-lg:flex-col max-md:p-10 max-sm:p-10 max-xl:p-20'>
+                <div className='flex-5 flex flex-col max-w-[1000px] gap-5'>
+                    <div className='flex items-center gap-2'>
+                        {/* garis */}
+                        <div className='w-10 h-[2px] bg-white'></div>
+                        <h2 className='text-xl'>About Us</h2>
                     </div>
-                    <h2>LUXURY GARAGE DOORS</h2>
+                    <h1 className='text-3xl font-medium uppercase leading-tight'>LUXURY GARAGE DOORS</h1>
+                    <p className='text-md leading-9 font-light max-w-[600px]'>
+                        LuxuryGarageDoors, founded in 2009 in Ontario, is a premier luxury garage door service provider. Specializing in residential, commercial, and industrial doors, our skilled professionals operate 24/7 through five units in Southern Ontario. With a stellar reputation and over 5,060 completed projects in the GTA, we prioritize client satisfaction, recognized by the Canadian Door Institute and the Better Business Bureau.
+                    </p>
+                    <button className='flex justify-center gap-2 items-center max-w-[220px] h-61 rounded-full bg-[#D74339] bg-opacity-90 px-4 py-2 hover:bg-opacity-50 transition duration-300'>
+                        <p className='text-white font-medium'>Know Us More</p>
+                        <FaCircleArrowRight className='text-white' />
+                    </button>
                 </div>
-
-                <div className=''>
-
+                <div className='flex-1 flex justify-center w-full '>
+                    <img src={Garagedoorman} alt='garage door' className='max-w-[500px] max-h-[500px] max-xl:max-w-[400px] max-xl:max-h-[400px] object-cover' />
                 </div>
             </div>
         </div>
