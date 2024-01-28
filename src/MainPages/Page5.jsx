dcabf7d3f804508b9b9c793339528e517cbb9b74
Hadad Al Akbar <75150256+qweersq@users.noreply.github.com>
2024-01-28 20:42:40 +0700

    add page5

--- src/MainPages/Page5.jsx
+++ src/MainPages/Page5.jsx
@@ -9,8 +9,8 @@ import BgCardService3 from '../assets/images/card-service-3.png';
 function Page5() {
     return (
         <div>
-            <div className='bg-[#2A2A2A] grid grid-cols-3 gap-4 flex gap-10 text-white p-40 max-lg:flex-col max-md:p-10 max-sm:p-10 max-xl:p-20'>
-                <div className='col-span-3 flex-5 flex flex-col max-w-[1000px] gap-5'>
+            <div className='bg-[#2A2A2A] grid grid-cols-3 max-md:grid-cols-6 gap-4 flex gap-10 text-white p-40 max-lg:flex-col max-md:p-10 max-sm:p-10 max-xl:p-20'>
+                <div className='col-span-3 max-md:col-span-6 flex-5 flex flex-col max-w-[1000px] gap-5'>
                     <div className='flex items-center gap-2'>
                         {/* garis */}
                         <div className='w-10 h-[2px] bg-white'></div>
@@ -18,15 +18,48 @@ function Page5() {
                     </div>
                     <h1 className='text-3xl font-medium uppercase leading-tight'>FIND OUT MORE ABOUT OUR SERVICES</h1>
                 </div>
-                <div className='flex-5 flex'>
-                    <div className="relative bg-gray-300 p-4 shadow-md rounded-xl flex flex-col gap-1">
+                <div className='flex justify-center max-md:col-span-3 max-sm:col-span-6'>
+                    <div className="relative flex flex-col p-5 justify-end h-[400px] w-[350px] max-sm:w-full transition-transform transform hover:brightness-110">
                         {/* Background */}
-                        <img src={BgCardService1} alt={`background`} className="absolute inset-0 w-full h-full object-cover rounded-xl" />
+                        <img src={BgCardService1} alt={`background`} className="absolute max-sm:w-full inset-0 h-full object-cover rounded-xl " />
 
                         {/* Teks yang dapat ditimpa di depan background */}
-                        <div className="relative z-10">
-                            <h1 className='text-lg text-white'>TEST</h1>
-                            <p className='text-sm font-light text-white'>adsfasdf</p>
+                        <div className="relative z-10 flex flex-col gap-3">
+                            <h1 className='text-2xl text-white font-bold'>Professional Installation Services</h1>
+                            <button className='flex justify-center gap-2 items-center max-w-[220px] h-61 rounded-full bg-[#D74339] bg-opacity-70 px-4 py-2 hover:bg-opacity-90 transition duration-300'>
+                                <p className='text-white font-medium'>More Info</p>
+                                <FaCircleArrowRight className='text-white' />
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                <div className='flex justify-center max-md:col-span-3 max-sm:col-span-6'>
+                    <div className="relative flex flex-col p-5 justify-end h-[400px] w-[350px] max-sm:w-full transition-transform transform hover:brightness-110">
+                        {/* Background */}
+                        <img src={BgCardService2} alt={`background`} className="absolute max-sm:w-full inset-0 h-full object-cover rounded-xl " />
+
+                        {/* Teks yang dapat ditimpa di depan background */}
+                        <div className="relative z-10 flex flex-col gap-3">
+                            <h1 className='text-2xl text-white font-bold'>Maintenance Service</h1>
+                            <button className='flex justify-center gap-2 items-center max-w-[220px] h-61 rounded-full bg-[#D74339] bg-opacity-70 px-4 py-2 hover:bg-opacity-90 transition duration-300'>
+                                <p className='text-white font-medium'>More Info</p>
+                                <FaCircleArrowRight className='text-white' />
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                <div className='flex justify-center max-md:col-span-3 max-sm:col-span-6 '>
+                    <div className="relative flex flex-col p-5 justify-end h-[400px] w-[350px] max-sm:w-full transition-transform transform hover:brightness-110">
+                        {/* Background */}
+                        <img src={BgCardService3} alt={`background`} className="absolute inset-0 h-full max-sm:w-full object-cover rounded-xl " />
+
+                        {/* Teks yang dapat ditimpa di depan background */}
+                        <div className="relative z-10 flex flex-col gap-3">
+                            <h1 className='text-2xl text-white font-bold'>24/7 Repair Assistance</h1>
+                            <button className='flex justify-center gap-2 items-center max-w-[220px] h-61 rounded-full bg-[#D74339] bg-opacity-70 px-4 py-2 hover:bg-opacity-90 transition duration-300'>
+                                <p className='text-white font-medium'>More Info</p>
+                                <FaCircleArrowRight className='text-white' />
+                            </button>
                         </div>
                     </div>
                 </div>
