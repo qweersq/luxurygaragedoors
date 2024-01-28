d0ecc6a8029bd5717e48f5a1adbbccd56f1651fb
Hadad Al Akbar <75150256+qweersq@users.noreply.github.com>
2024-01-28 16:03:16 +0700

    update page 1

--- src/MainPages/Page1.jsx
+++ src/MainPages/Page1.jsx
@@ -4,17 +4,20 @@ import Bg1 from '../assets/images/background1.png';
 function Page1(props) {
     return (
         <div>
-            <div className='w-full'>
-                <img src={Bg1} alt='Background 1' className='z-[-10] w-full h-[800px] max-sm:h-[700px]  object-cover' />
+            <div className='relative w-full h-[800px] max-sm:h-[700px]'>
+                <img src={Bg1} alt='Background 1' className='w-full h-full object-cover' />
 
                 {/* Text */}
-                <div className='mt-[-500px] flex flex-col gap-4 justify-center items-center max-sm:mt-[-570px] px-10'>
-                    <h1 className='text-center text-white font-poppins text-5xl font-medium uppercase leading-tight'>Elevate Your Space with
+                <div className='absolute inset-0 flex flex-col justify-center items-center px-10 text-center text-white'>
+                    <h1 className='font-poppins text-5xl font-medium uppercase leading-tight'>
+                        Elevate Your Space with
                         <span className='text-[#BD261F] ml-3'>Luxury Garage Doors</span>
                     </h1>
-                    <p className='text-white opacity-60 text-center font-poppins text-lg font-medium max-w-[1000px]'>Your Premier Garage Door Service and Repair Solution, delivering top-notch expertise and unmatched reliability for all your garage door needs!</p>
+                    <p className='opacity-60 font-poppins text-lg font-medium max-w-[1000px]'>
+                        Your Premier Garage Door Service and Repair Solution, delivering top-notch expertise and unmatched reliability for all your garage door needs!
+                    </p>
 
-                    <div className='flex gap-3 items-center justify-center'>
+                    <div className='flex gap-3 items-center justify-center mt-6'>
                         <button className='w-190 h-61 rounded-full bg-white bg-opacity-90 px-4 py-2 hover:bg-opacity-50 transition duration-300'>
                             <p className='text-black font-poppins font-medium'>About Us</p>
                         </button>
@@ -24,6 +27,7 @@ function Page1(props) {
                     </div>
                 </div>
             </div>
+
         </div>
     );
 }
---
+++ src/MainPages/Page3.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function Page3() {
+    return (
+        <div>
+            <div className='bg-[#2A2A2A] flex gap-10'>
+                <div className='flex flex-col'>
+                    <div>
+                        <span className="w-83 h-3 bg-gray-300"></span>
+                        <h3>About Us</h3>
+                    </div>
+                    <h2>LUXURY GARAGE DOORS</h2>
+                </div>
+
+                <div className=''>
+
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Page3;
