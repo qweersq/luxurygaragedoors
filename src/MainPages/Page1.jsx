9f244c3971510df168e2de9b5e56e0c65ccdfb6e
Hadad Al Akbar <75150256+qweersq@users.noreply.github.com>
2024-01-28 15:52:39 +0700

    finished page 1

--- src/MainPages/Page1.jsx
+++ src/MainPages/Page1.jsx
@@ -4,16 +4,25 @@ import Bg1 from '../assets/images/background1.png';
 function Page1(props) {
     return (
         <div>
-            <div className='w-full '>
-                <img src={Bg1} alt='Background 1' className='z-[-10] w-full h-[800px] max-sm:h-[500px] object-cover' />
+            <div className='w-full'>
+                <img src={Bg1} alt='Background 1' className='z-[-10] w-full h-[800px] max-sm:h-[700px]  object-cover' />
 
                 {/* Text */}
-                <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-center'>
-                    <h1 className='text-6xl font-bold'>Garage Door Repair & Installation</h1>
-                    <p className='text-2xl'>We are a family owned and operated business, with over 20 years experience in the garage door industry.</p>
-                    <button className='bg-[#FFD700] text-black p-2 rounded-lg mt-5'>Get a Free Quote</button>
+                <div className='mt-[-500px] flex flex-col gap-4 justify-center items-center max-sm:mt-[-570px] px-10'>
+                    <h1 className='text-center text-white font-poppins text-5xl font-medium uppercase leading-tight'>Elevate Your Space with
+                        <span className='text-[#BD261F] ml-3'>Luxury Garage Doors</span>
+                    </h1>
+                    <p className='text-white opacity-60 text-center font-poppins text-lg font-medium max-w-[1000px]'>Your Premier Garage Door Service and Repair Solution, delivering top-notch expertise and unmatched reliability for all your garage door needs!</p>
 
+                    <div className='flex gap-3 items-center justify-center'>
+                        <button className='w-190 h-61 rounded-full bg-white bg-opacity-90 px-4 py-2 hover:bg-opacity-50 transition duration-300'>
+                            <p className='text-black font-poppins font-medium'>About Us</p>
+                        </button>
+                        <button className='w-190 h-61 rounded-full border border-white bg-opacity-90 px-4 py-2 hover:bg-gray-800 hover:border-gray-800 transition duration-300'>
+                            <p className='text-white font-poppins font-medium hover:opacity-50 transition duration-300'>Request A Quote</p>
+                        </button>
                     </div>
+                </div>
             </div>
         </div>
     );
